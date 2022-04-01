3bb5a403395239c362d1a9a98846183d6e5ca3c7
GAMariaF <93644761+GAMariaF@users.noreply.github.com>
2022-04-01 12:54:23 +0200

    get_run_id

--- db/main_db.py
+++ db/main_db.py
@@ -6,6 +6,7 @@ from vcfutils import filter_nocalls
 from vcfutils import explode_format_gt
 from vcfutils import explode_info
 from vcfutils import get_sample_id
+from vcfutils import get_run_id
 from dbutils import populate_db
 
 vcffile = './Oncomine_Variants_(5.16)_filtered.vcf'
@@ -16,8 +17,9 @@ df = explode_format_gt(df)
 df = explode_info(df)
 
 #df.to_csv('test.csv')
-
+db = '/illumina/analysis/dev/2022/mfahls/fullFres/fullFres/db/variant.db'
+run_id = get_run_id(vcffile)
 sample_id = get_sample_id(vcffile)
-print(sample_id)
+print(run_id, sample_id)
 
 #populate_db(db, df, run_id, sample_id)
