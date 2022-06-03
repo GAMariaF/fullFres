5a1790e346a5e12f763afb1c0fa5d46384845eee
oyvindbusk@gmail.com <oyvindbusk@gmail.com>
2022-06-03 11:16:40 +0200

    check if in docker container

--- run_backend_server.py
+++ run_backend_server.py
@@ -1,4 +1,6 @@
-import sys
+import sys, os
+
+SECRET_KEY = os.environ.get('AM_I_IN_A_DOCKER_CONTAINER', False)
 
 # To funksjoner for aa sjekke om virtualenv er aktivert:
 def get_base_prefix_compat():
@@ -8,7 +10,13 @@ def get_base_prefix_compat():
 def in_virtualenv():
     return get_base_prefix_compat() != sys.prefix
 
-if not in_virtualenv():
+def in_docker():
+    if SECRET_KEY:
+        return True
+    else:
+        return False
+
+if not in_virtualenv() or not in_docker():
     print("\n\n--Har du husket aa aktivere virtualenv--\n\n--source env/bin/activate--\n")
     exit()
 
@@ -16,6 +24,8 @@ from backend import app
 
 if __name__ == '__main__':
     print( "in_virtualenv()")
-    app.run('172.16.0.3', port=5000, threaded=True, debug=True)
+    app.run(port=5000, threaded=True, debug=True)
+    #app.run('172.16.0.3', port=5000, threaded=True, debug=True)
+    
 
     
