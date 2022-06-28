3abf4823d0069d0b7a9ad98ee0e4f9da1621035c
GAMariaF <93644761+GAMariaF@users.noreply.github.com>
2022-06-28 14:40:35 +0200

    .

--- frontend/src/appUtils.js
+++ frontend/src/appUtils.js
@@ -15,18 +15,6 @@ var util_funcs = {
         const baseURI = url + '/api/' + query
         return axios.get(baseURI)
         .then(function (response) {return response.data});      
-    },
-    sort_table(objects){
-        console.log("sort_table")
-        // const order = ["Locus","Ref","ALTEND","gene"];
-        // const orderIndex = {}
-        // order.forEach((value,index) => orderIndex[value]=index);
-        // console.log(orderIndex)
-        // // //const sortable = Object.entries(objects)
-        //console.log(objects[sortable])
-        // Sort
-        //objects.sort((a,b) => orderIndex[a.CHROM] - orderIndex[b.CHROM]);
-        return objects
     }
 };
 
