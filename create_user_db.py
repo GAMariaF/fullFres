530a2fb8468a982ea612da6b3caedec36810bf93
oyvindbusk@gmail.com <oyvindbusk@gmail.com>
2022-08-18 13:20:17 +0200

    Added some users. Closes #47

--- create_user_db.py
+++ create_user_db.py
@@ -16,5 +16,14 @@ db.create_all()
 hashed_password = generate_password_hash('buso123', method='sha256')
 db.session.add(Users(public_id=str(uuid.uuid4()), name='buso', password=hashed_password, admin=True))
 
+hashed_password = generate_password_hash('strali123', method='sha256')
+db.session.add(Users(public_id=str(uuid.uuid4()), name='strali', password=hashed_password, admin=True))
+
+hashed_password = generate_password_hash('mfahl123', method='sha256')
+db.session.add(Users(public_id=str(uuid.uuid4()), name='mfahl', password=hashed_password, admin=True))
+
+hashed_password = generate_password_hash('stauj123', method='sha256')
+db.session.add(Users(public_id=str(uuid.uuid4()), name='stauj', password=hashed_password, admin=True))
+
 db.session.commit()
 
