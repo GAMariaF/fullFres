999b79aa9791abe5cb51f7e24625afacb3df121c
sigvla <Sigve.Strand.Landa@sthf.no>
2023-01-10 18:18:17 +0100

    Minor changes

--- run_backend_server.py
+++ run_backend_server.py
@@ -24,7 +24,6 @@ from backend import app
 
 if __name__ == '__main__':
     print( "in_virtualenv()")
-    app.run('0.0.0.0',port=5000, threaded=True, debug=True)
-    #app.run('172.16.0.3', port=5000, threaded=True, debug=True)
+    app.run('172.16.0.3', port=5000, threaded=True, debug=True)
     
    
