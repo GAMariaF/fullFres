07f69f4d0ef95b5eaabc317d4675cb0d39cd633a
sigvla <Sigve.Strand.Landa@sthf.no>
2023-03-23 13:25:40 +0100

    Bug fixes

--- docker_startup_cmd.sh
+++ docker_startup_cmd.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
-cd /fullFres/frontend && pm2 start "npx http-server dist/ --port 8080" --name frontend
+cd /fullFres/frontend && pm2 start "npx http-server dist/ --proxy http://localhost:8081?" --name frontend
 cd /fullFres && python3 run_backend_server.py
