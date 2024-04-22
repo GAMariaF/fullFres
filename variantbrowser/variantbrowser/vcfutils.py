e5692c120c678b5030e235fdeefe701aa6cb64ec
sigvla <Sigve.Strand.Landa@sthf.no>
2024-04-22 13:43:10 +0200

    Fix CNV insertion

--- variantbrowser/variantbrowser/vcfutils.py
+++ variantbrowser/variantbrowser/vcfutils.py
@@ -23,32 +23,30 @@ def parse_thermo_vcf(vcf,excel):
     if not df_excel_wf.empty:
         df_excel_wf = df_excel_wf.assign(ID = df_excel_wf.loc[:,'Variant ID'] + "_1")
         df1 = pd.merge(df_excel_wf,df_vcf,on='ID',how='left')
-        df1.loc[:,'ID']=df1.loc[:,'Variant ID']
+        df1.loc[:,'ID'] = df1.loc[:,'Variant ID']
     # With RNAExonVariant
     df_excel_wRNA = df_excel.loc[df_excel['Type'] == 'RNAExonVariant']
     if not df_excel_wRNA.empty:
         df_excel_wRNA = df_excel_wRNA.assign(ID = df_excel_wRNA.loc[:,'Variant ID'] + "_1")
         df3 = pd.merge(df_excel_wRNA,df_vcf,on='ID',how='left')
-        df3.loc[:,'ID']=df3.loc[:,'Variant ID']
+        df3.loc[:,'ID'] = df3.loc[:,'Variant ID']
     
+    df_vcf["Locus_vcf"] = df_vcf.CHROM.astype(str) + ":" + df_vcf.POS.astype(str)
     # With CNVs
     df_excel_wc = df_excel.loc[df_excel['Type'] == 'CNV']
     if not df_excel_wc.empty:
         df_excel_wc = df_excel_wc.reset_index(drop='True')
-        df_vcf["Locus_vcf"] = df_vcf.CHROM.astype(str)+":" \
-                    +df_vcf.POS.astype(str)
-        # Fix so they will always be unique.
-        df2 = pd.merge(df_excel_wc,df_vcf,\
+        df4 = pd.merge(df_excel_wc,df_vcf,\
                     left_on=['Locus','Variant ID'],right_on=['Locus_vcf','ID'],how='left')
+        df4 = df4.drop(columns=['Locus_vcf'])
 
     # Without fusion RNAExonVariants and CNVs
     df_excel_wo = df_excel.loc[df_excel['Type'] != 'Fusion']
     df_excel_wo = df_excel_wo.loc[df_excel_wo['Type'] != 'RNAExonVariant']
     df_excel_wo = df_excel_wo.loc[df_excel_wo['Type'] != 'CNV']
+    
     if not df_excel_wo.empty:
         df_excel_wo = df_excel_wo.reset_index(drop='True')
-        df_vcf["Locus_vcf"] = df_vcf.CHROM.astype(str)+":" \
-                    +df_vcf.POS.astype(str)
         # Fix so they will always be unique.
         df2 = pd.merge(df_excel_wo,df_vcf,\
                     left_on=['Locus','Variant ID', "Ref", "Alt"],right_on=['Locus_vcf', 'ID', "REF", "ALT"],how='left')
